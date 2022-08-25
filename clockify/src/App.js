b5f48c3e8b72c30c11ae0b05b1c50292cceee4aa
Raghav435 <shuklaraghav435@gmail.com>
2022-08-25 15:02:54 +0530

    files

--- clockify/src/App.js
+++ clockify/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<SignUp/>}/>    
       </Routes>
-      {/* <Download/> */}
+      <Download/>
     </div>
   );
 }
