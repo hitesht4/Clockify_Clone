bea5b0c0b8bc64672740fa5bba1b331c5cdcd591
hitesht4 <97459069+hitesht4@users.noreply.github.com>
2022-08-26 22:54:56 +0530

    day4p2

--- clockify/src/Components/TimerApp/TaskComponent.jsx
+++ clockify/src/Components/TimerApp/TaskComponent.jsx
@@ -1,42 +1,64 @@
-import React from 'react';
+import React,{ useState} from 'react';
 import styles from './Styles/TaskComp.module.css';
 import {GrAddCircle} from 'react-icons/gr';
-import {BsFillTagsFill,BsCalendar4Event,BsThreeDotsVertical} from 'react-icons/bs';
-import {FiDollarSign,FiPlay} from 'react-icons/fi';
+import {BsFillTagsFill,BsThreeDotsVertical} from 'react-icons/bs';
+import {FiDollarSign} from 'react-icons/fi';
+import {AiTwotoneEdit} from 'react-icons/ai';
+import {MdDeleteForever} from 'react-icons/md';
+import { useDispatch } from 'react-redux';
+import { deleteTasksApi, updateTaskNameApi, updateTasksApi } from '../../Redux/Tasks/Task.actions';
 
-const TaskComponent = () => {
+const TaskComponent = ({item}) => {
+    const [open,setOpen]=useState(false);
+    const [task,setTask]=useState("");
+    const dispatch=useDispatch();
+    const handleDelete=()=>{
+      dispatch(deleteTasksApi(item.id));
+    }
+    const handleStatus=()=>{
+      dispatch(updateTasksApi(item.id,item));  
+    }
+    const handleClick=(e)=>{
+      setTask(e.target.value);
+    }
+    const handlePress=(e)=>{
+      if(e.key==="Enter"){
+        dispatch(updateTaskNameApi(item.id,task));
+        setOpen(false);
+      }
+      
+    }
   return (
     <div className={styles.Task}>
-       <div className={styles.Desc}>
-            {/* <input type="text" value="Item"/> */}
-            <div>
-                <h6>Item</h6>
+       <div className={styles.Desc} >
+            <div onClick={()=>setOpen(!open)} className={styles.Up}>
+               {open ? <input type="text" autoFocus onChange={handleClick} onKeyDown={handlePress}/>:  <h6 style={{color:item.status ? "green" : "red"}}>{item.name}</h6>}    
             </div>
             <div className={styles.Projects}>
             <GrAddCircle style={{fontSize:"20px",color:"#1cb9f7"}}/>
                 <h6>Project</h6>
             </div>
             <div className={styles.Tags}>
-                <BsFillTagsFill style={{fontSize:"30px"}}/>
+                <BsFillTagsFill style={{fontSize:"22px"}}/>
             </div>
             <div className={styles.Tags}>
-                <FiDollarSign style={{fontSize:"30px"}}/>
+                <FiDollarSign style={{fontSize:"22px"}}/>
             </div>
             <div className={styles.Time}>
-            <input type="text" />
-            <input type="text" />
+            <h6>{item.start}</h6>
+            <h6>{item.end}</h6>
             </div>
             <div className={styles.Edit}>
-                <h6>Timer</h6>
+                <h6></h6>
+            </div>
+            <div className={styles.Tags3} onClick={handleStatus}>
+                <AiTwotoneEdit style={{fontSize:"22px"}}/>
+            </div>
+            <div className={styles.Tags2} onClick={handleDelete}>
+                <MdDeleteForever style={{fontSize:"25px"}}/>
             </div>
             <div className={styles.Tags}>
-                <BsCalendar4Event style={{fontSize:"30px"}}/>
-            </div>
-            <div className={styles.Tags}>
-                <FiPlay style={{fontSize:"30px"}}/>
-            </div>
-            <div className={styles.Tags}>
-                <BsThreeDotsVertical style={{fontSize:"30px"}}/>
+                <BsThreeDotsVertical style={{fontSize:"22px"}}/>
             </div>
        </div>
     </div>
--- clockify/src/Components/TimerApp/TasksApp.jsx
+++ clockify/src/Components/TimerApp/TasksApp.jsx
@@ -5,26 +5,50 @@ import {useSelector,useDispatch} from 'react-redux';
 import TaskComponent from './TaskComponent';
 import Heading from './Heading';
 import { getTasksApi } from '../../Redux/Tasks/Task.actions';
+import styles from './Styles/TaskApp.module.css';
+import EmptyTask from './EmptyTask';
 
 const TasksApp = () => {
 
-   const {tasks}=useSelector((state)=>state.tasks);
+   const {tasks,loading,error}=useSelector((state)=>state.tasks);
    const dispatch=useDispatch();
 
  useEffect(()=>{
    dispatch(getTasksApi());
  },[]);
 
- 
+ if(loading){
   return (
-    <div>
+  <div className={styles.Body1}>
     <TaskInput/>
+    <h1 className={styles.Load}>Loading....</h1>
+  </div>
+  )
+ }
+  else if(error){
+    return (
+    <div className={styles.Body1}>
+      <TaskInput/>
+      <h1 className={styles.Load2}>Error....</h1>
+    </div>
+    )
+
+  
+ }else{
+  return (
+    <div className={styles.Body1}>
+      <TaskInput/>
+    {tasks.length<=0 ?
+    <div><EmptyTask/></div>:
+    <div >
     <Heading/>
     {tasks.map(item=>(
-       <TaskComponent key={item.id} {...item}/>
+       <TaskComponent key={item.id} item={item}/>
     ))}
-    </div>
+    </div>}
+   </div>
   )
+}
 }
 
 export default TasksApp
