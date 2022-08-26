bfd2ab7db0cf1aeff9565b7cb7c1df010b2787c2
prabhattopi <topi9864@gmail.com>
2022-08-26 13:15:00 +0530

    The file

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -13,7 +13,7 @@ const User=require("../models/userModels")
 
 const registerUser=asyncHandler(async(req,res)=>{
     const {email,password}=req.body
-    if(!name||!email||!password){
+    if(!email||!password){
         res.status(400)
         throw new Error("Please add all fields")
     }
--- clockify/src/index.js
+++ clockify/src/index.js
@@ -5,16 +5,24 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider as ReduxProvider} from 'react-redux';
+import { Provider as PrabhatProvider } from 'react-redux';
 import {Store} from '../src/Redux/Store';
+import {store} from "../src/app/store"
+import { UserAuthContextProvider } from './context/UserAuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <UserAuthContextProvider>
+    <PrabhatProvider store={store}>
+
     <ReduxProvider store={Store}>
     <App />
     </ReduxProvider>
+    </PrabhatProvider>
+    </UserAuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
