81098c9d04a97ba1f67e0892c0bdfa89e286022b
Hitesh Tarwani <hiteshtarwani4@gmail.com>
2023-04-16 19:35:06 +0530

    backend correction

--- clockify/src/features/auth/authserver.js
+++ clockify/src/features/auth/authserver.js
@@ -1,31 +1,31 @@
-import axios from "axios"
+import axios from "axios";
 
-const API_URL=`/api/users/`
+const API_URL = `http://localhost:5000/api/users/`;
 
 //Register  User
-const register=async(userData)=>{
-    const response=await axios.post(API_URL,userData)
-    if(response.data){
-        localStorage.setItem("User",JSON.stringify(response.data))
-    }
-    return response.data
-}
+const register = async (userData) => {
+  const response = await axios.post(API_URL, userData);
+  console.log(response);
+  if (response.data) {
+    localStorage.setItem("User", JSON.stringify(response.data));
+  }
+  //   return response.data;
+};
 //Login  User
-const login=async(userData)=>{
-    const response=await axios.post(API_URL+"login",userData)
-    if(response.data){
-        localStorage.setItem("User",JSON.stringify(response.data))
-    }
-    return response.data
-}
-const logout=()=>{
-    localStorage.removeItem("User")
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "login", userData);
+  if (response.data) {
+    localStorage.setItem("User", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+const logout = () => {
+  localStorage.removeItem("User");
+};
 
-}
-
-const authService={
-    register,
-    logout,
-    login,
-}
-export default authService
+const authService = {
+  register,
+  logout,
+  login,
+};
+export default authService;
--- server/src/controller/goalController.js
+++ server/src/controller/goalController.js
@@ -1,38 +1,25 @@
 const asyncHandler = require("express-async-handler");
-
 const Goal = require("../models/goalModel");
-const User=require("../models/userModels")
-//@des Get goals
-//@route GET /api/goals
-//@access Private
 
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find({user:req.user.id});
+  const goals = await Goal.find({ user: req.user.id });
   res.status(200).json(goals);
 });
 
-//@des Set goals
-//@route POST /api/goals
-//@access Private
-
 const setGoals = asyncHandler(async (req, res) => {
-  if (!req.body.text||!req.body.start||!req.body.end) {
+  if (!req.body.text || !req.body.start || !req.body.end) {
     res.status(400);
     throw new Error("Please add a text field");
   }
   const goal = await Goal.create({
     text: req.body.text,
-    start:req.body.start,
-    end:req.body.end,
-    status:req.body.status,
-    user:req.user.id
+    start: req.body.start,
+    end: req.body.end,
+    status: req.body.status,
+    user: req.user.id,
   });
   res.status(200).json(goal);
 });
-
-//@des Updatet goals
-//@route PUT /api/goals/:id
-//@access Private
 
 const updateGoals = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
@@ -42,15 +29,14 @@ const updateGoals = asyncHandler(async (req, res) => {
   }
 
   //check for user
-  if(!req.user){
-    res.status(400)
-    throw new Error("User not found")
+  if (!req.user) {
+    res.status(400);
+    throw new Error("User not found");
   }
   //Make sure the logged in user matches the goal user
-  if(goal.user.toString()!==req.user.id){
-    res.status(401)
-    throw new Error("User not authorized")
-
+  if (goal.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
   const updateGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -58,30 +44,25 @@ const updateGoals = asyncHandler(async (req, res) => {
   res.status(200).json(updateGoal);
 });
 
-//@des delete goals
-//@route DELETE /api/goals/:id
-//@access Private
-
 const deleteGoals = asyncHandler(async (req, res) => {
   const goal = await Goal.findByIdAndRemove(req.params.id);
-  if(!goal){
-    res.status(400)
-    throw new Error("Goal not Found")
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not Found");
   }
-  
+
   //check for user
-  if(!req.user){
-    res.status(400)
-    throw new Error("User not found")
+  if (!req.user) {
+    res.status(400);
+    throw new Error("User not found");
   }
   //Make sure the logged in user matches the goal user
-  if(goal.user.toString()!==req.user.id){
-    res.status(401)
-    throw new Error("User not authorized")
-
+  if (goal.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
-  
-  res.status(200).json({id:req.params.id});
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
